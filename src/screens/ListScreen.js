58ec376e49e961d9e3a30149611b4fd4aafbd2ab
fritzgt <tuanis@live.com>
2020-09-27 13:23:49 +0300

    add margin to list

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { Text, View, FlatList } from 'react-native';
+import { Text, View, FlatList, StyleSheet } from 'react-native';
 
 const ListScreen = () => {
   const friends = [{ name: 'Fritz' }, { name: 'Sasha' }, { name: 'Kristen' }];
   return (
     <FlatList
+      keyExtractor={(friend) => friend.name}
       data={friends}
       renderItem={({ item }) => {
-        return <Text>{item.name}</Text>;
+        return <Text style={styles.textStyle}>{item.name}</Text>;
       }}
     />
   );
 };
+
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 30,
+    backgroundColor: 'red',
+  },
+});
 
 export default ListScreen;
