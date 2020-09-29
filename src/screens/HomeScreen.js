92e86afb6c564da0151d87361e89d70d80f38e3c
fritzgt <tuanis@live.com>
2020-09-29 11:46:21 +0300

    added contear layout

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,34 +1,36 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = (props) => {
+const HomeScreen = ({ navigation }) => {
   const [counter, setCounter] = useState(0);
 
   const navigate = () => {
-    props.navigation.navigate('List');
+    navigation.navigate('List');
   };
   return (
     <View>
-      <Text style={styles.text}>Counter: {counter}</Text>
-      <Button
-        style={styles.spacing}
-        onPress={() => setCounter(counter - 1)}
-        title="Decrease counter"
-      />
-      <TouchableOpacity
-        style={styles.spacing}
-        onPress={() => setCounter(counter + 1)}
-      >
-        <Text>Increase counter</Text>
-      </TouchableOpacity>
+      <View style={styles.container}>
+        <Button title="+" onPress={() => setCounter(counter + 1)} />
+        <Text style={styles.text}>Counter: {counter}</Text>
+        <Button
+          onPress={() => (counter <= 0 ? '' : setCounter(counter - 1))}
+          title="-"
+        />
+      </View>
+
       <Button title="Go to List" onPress={() => navigate()} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   text: {
-    fontSize: 50,
+    fontSize: 20,
     marginVertical: 50,
   },
   spacing: {
